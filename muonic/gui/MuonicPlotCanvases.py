7d245618d438a504aa79512068fe4f4792181811
basho.got <basho.got@1666bff2-7c83-11de-a0b6-3f18d6cea68c>
2013-07-10 11:33:47 +0000

    Binning in velocity plot changed to the double - hope that solves the resolution problem for the right shoulder of the hist

--- muonic/gui/MuonicPlotCanvases.py
+++ muonic/gui/MuonicPlotCanvases.py
@@ -365,7 +365,7 @@ class VelocityCanvas(MuonicHistCanvas):
     
     def __init__(self,parent,logger): 
         #super(VelocityCanvas,self).__init__(self,parent,logger,binning=n.linspace(0.7,1.3,20),xmin=0.8,xmax=1.2,ymin=0,ymax=2,ylabel="events",xlabel="muon velocity (c)") 
-        MuonicHistCanvas.__init__(self,parent,logger,n.linspace(0.,2,20),xmin=0.7,xmax=1.3,ymin=0,ymax=2,ylabel="events",xlabel="muon velocity (c)") 
+        MuonicHistCanvas.__init__(self,parent,logger,n.linspace(0.,2,10),xmin=0.7,xmax=1.3,ymin=0,ymax=2,ylabel="events",xlabel="muon velocity (c)") 
         self.dimension = r"$c$"
         
 class PulseWidthCanvas(MuonicHistCanvas):     
