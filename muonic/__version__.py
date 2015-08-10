09ed2bc7e0cd9cc13ead690ae7988c97ea0303f7
Hans-Peter Bretz <hans-peter.bretz@desy.de>
2015-08-10 17:22:34 +0200

    Upped version string.

--- muonic/__version__.py
+++ muonic/__version__.py
@@ -1,4 +1,4 @@
-__version__ = "2.0.21"
+__version__ = "2.0.22"
 __source_location__ = "https://github.com/achim1/muonic/"
 __docs_hosted_at__  = "http://achim1.github.io/muonic/"
 __manual_hosted_at__ = "http://physik-begreifen-zeuthen.desy.de/angebote/kosmische_teilchen/schuelerexperimente/cosmo_experiment/index_ger.html"
