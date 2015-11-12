b671cb11391e8e546f2e7d3019d0f8bd18e233f8
Hans-Peter Bretz <hans-peter.bretz@desy.de>
2015-11-12 15:30:46 +0100

    Fixed error in code that showed the wrong fit parameter in the legend of the muon decay histogram.

--- muonic/gui/MuonicPlotCanvases.py
+++ muonic/gui/MuonicPlotCanvases.py
@@ -382,7 +382,7 @@ class MuonicHistCanvas(MuonicPlotCanvas):
         perr_leastsq = n.array(error)
         try:
             #self.ax.legend(("Data","Fit: (%4.2f +- %4.2f) %s  \n chisq/ndf=%4.2f"%(p[1],n.sqrt(covar[1][1]),self.dimension,chisquare/(nbins-len(p)))),loc=1)
-            self.ax.legend(("Data","Fit: (%4.2f $\pm$ %4.2f) %s  \n chisq/ndf=%4.2f"%(p[2],perr_leastsq[2],self.dimension,chisquare/(nbins-len(p)))),loc=1)
+            self.ax.legend(("Data","Fit: (%4.2f $\pm$ %4.2f) %s  \n chisq/ndf=%4.2f"%(p[1],perr_leastsq[2],self.dimension,chisquare/(nbins-len(p)))),loc=1)
         except TypeError:
             self.logger.warn('Covariance Matrix is None, could not calculate fit error!')
             self.ax.legend(("Data","Fit: (%4.2f) %s \n chisq/ndf=%4.2f"%(p[1],self.dimension,chisquare/(nbins-len(p)))),loc=1)
